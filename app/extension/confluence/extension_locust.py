8ba18e03bdc0961ef5639fff1a15b7274a593de6
Rytis <rytis@mohami.io>
2020-11-07 11:50:49 +0200

    dfdf

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -6,23 +6,13 @@ logger = init_logger(app_type='confluence')
 
 @confluence_measure
 def app_specific_action(locust):
-    r = locust.get('/app/get_endpoint', catch_response=True)  # call app-specific GET endpoint
+    r = locust.get(f'/rest/newsteaser/1.0/news/search?cql=(type%20IN%20(blogpost))&expand=body.view,body.view.webresource.uris.all,children.attachment,children.comment,space,version,metadata.likes,history', catch_response=True)  # call app-specific GET endpoint
     content = r.content.decode('utf-8')   # decode response content
 
-    token_pattern_example = '"token":"(.+?)"'
     id_pattern_example = '"id":"(.+?)"'
-    token = re.findall(token_pattern_example, content)  # get TOKEN from response using regexp
     id = re.findall(id_pattern_example, content)    # get ID from response using regexp
 
-    logger.locust_info(f'token: {token}, id: {id}')  # log information for debug when verbose is true in jira.yml file
-    if 'assertion string' not in content:
-        logger.error(f"'assertion string' was not found in {content}")
-    assert 'assertion string' in content  # assert specific string in response content
-
-    body = {"id": id, "token": token}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post('/app/post_endpoint', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'assertion string after successful POST request' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'assertion string after successful POST request' in content  # assertion after POST request
+    logger.locust_info(f'id: {id}')  # log information for debug when verbose is true in jira.yml file
+    if 'results' not in content:
+        logger.error(f"'results' was not found in {content}")
+    assert 'results' in content
