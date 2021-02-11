20fcbeea11e24e924c7ae39c3a03d566cec7b5ce
Rytis <rytis@mohami.io>
2021-02-11 12:18:49 +0200

    dfdfdsa

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -1,8 +1,9 @@
 import re
 import random
 
-from locustio.common_utils import init_logger, confluence_measure
+from locustio.common_utils import init_logger, confluence_measure, fetch_by_re
 from locustio.confluence.requests_params import confluence_datasets
+from locustio.confluence.requests_params import ViewPage
 
 confluence_dataset = confluence_datasets()
 logger = init_logger(app_type='confluence')
@@ -15,9 +16,14 @@ def app_specific_action(locust):
     page = random.choice(confluence_dataset["pages"])
     page_id = page[0]
     space_key = page[1]
-    atl_token = locust.session_data_storage['atl_token']
+    params = ViewPage()
 
-    body = {"atl_token": atl_token, "reverse": "false", "spaceCategoryNames": "", "includeArchivedSpaces": "false", "currentPageId": page_id, "currentSpaceKey": space_key, "currentSpaceTitle": ""}  # include parsed variables to POST request body
+    r = locust.get(f'/pages/viewpage.action?pageId={page_id}', catch_response=True)
+    content = r.content.decode('utf-8')
+
+    atl_token_view_issue = fetch_by_re(params.atl_token_view_issue_re, content)
+
+    body = {"atl_token": atl_token_view_issue, "reverse": "false", "spaceCategoryNames": "", "includeArchivedSpaces": "false", "currentPageId": page_id, "currentSpaceKey": space_key, "currentSpaceTitle": ""}  # include parsed variables to POST request body
     headers = {'content-type': 'application/json'}
     r = locust.post(f'/rest/spacetree/1.0/spaces', body, headers, catch_response=True)  # call app-specific POST endpoint
     content = r.content.decode('utf-8')
