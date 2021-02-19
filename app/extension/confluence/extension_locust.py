11e2507622de551cb6486e2c8620e74d24ef9461
Rytis <rytis@mohami.io>
2021-02-19 19:01:35 +0200

    dfdfdsa

--- app/extension/confluence/extension_locust.py
+++ app/extension/confluence/extension_locust.py
@@ -1,9 +1,5 @@
-import re
-import random
-
 from locustio.common_utils import init_logger, confluence_measure, fetch_by_re
 from locustio.confluence.requests_params import confluence_datasets
-from locustio.confluence.requests_params import ViewPage
 
 confluence_dataset = confluence_datasets()
 logger = init_logger(app_type='confluence')
@@ -13,20 +9,19 @@ confluence_dataset = confluence_datasets()
 
 @confluence_measure("locust_app_specific_action")
 def app_specific_action(locust):
-    page = random.choice(confluence_dataset["pages"])
-    page_id = page[0]
-    space_key = page[1]
-    params = ViewPage()
+    body = {"pageId": 41067214, "editorFormat": "<p>Page+edit+with+locust+ecruetvisb</p>"}  # include parsed variables to POST request body
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    r = locust.post(f'/rest/sourceeditor/1.0/convert/toStorageFormat', body, headers, catch_response=True)  # call app-specific GET endpoint
 
-    r = locust.get(f'/pages/viewpage.action?pageId={page_id}', catch_response=True)
-    content = r.content.decode('utf-8')
+    content = r.content.decode('utf-8')   # decode response content
 
-    atl_token_view_issue = fetch_by_re(params.atl_token_view_issue_re, content)
+    if 'content' not in content:
+        logger.error(f"'content' was not found in {content}")
+    assert 'content' in content
 
-    body = {"atl_token": atl_token_view_issue, "reverse": "false", "spaceCategoryNames": "@self", "includeArchivedSpaces": "false", "currentPageId": page_id, "currentSpaceKey": space_key, "currentSpaceTitle": ""}  # include parsed variables to POST request body
-    headers = {'content-type': 'application/json'}
-    r = locust.post(f'/rest/spacetree/1.0/spaces', body, headers, catch_response=True)  # call app-specific POST endpoint
-    content = r.content.decode('utf-8')
-    if 'spaceCategories' not in content:
-        logger.error(f"'assertion string after successful POST request' was not found in {content}")
-    assert 'spaceCategories' in content  # assertion after POST request
+    r = locust.post(f'/rest/sourceeditor/1.0/convert/toEditorFormat', body, headers, catch_response=True)  # call app-specific GET endpoint
+    content = r.content.decode('utf-8')   # decode response content
+
+    if 'content' not in content:
+        logger.error(f"'content' was not found in {content}")
+    assert 'content' in content
--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -3,21 +3,30 @@ from selenium_ui.conftest import print_timing
 from util.conf import CONFLUENCE_SETTINGS
 
 from selenium_ui.base_page import BasePage
-
+from selenium_ui.confluence.pages.pages import Login, AllUpdates, PopupManager, Page, Dashboard, TopNavPanel, Editor, \
+    Logout
 
 def app_specific_action(webdriver, datasets):
-    page = BasePage(webdriver)
-    if datasets['custom_pages']:
-        app_specific_page = datasets['custom_pages']
-        app_specific_page_id = app_specific_page[0][0]
+    edit_page = Editor(webdriver, page_id=datasets['page_id'])
 
     @print_timing("selenium_app_custom_action")
     def measure():
-
-        @print_timing("selenium_app_custom_action:view_page")
+        @print_timing("selenium_app_custom_action:view_source_editor")
         def sub_measure():
-            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
-            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
-            page.wait_until_visible((By.CLASS_NAME, "plugin_spacetree"))  # Wait for you app-specific UI element by ID selector
+            edit_page.go_to()
+            edit_page.wait_for_page_loaded()
+            edit_page.wait_until_visible((By.ID, "rte-source-editor-button"))  # Wait for title field visible
+            button = (By.ID, 'rte-source-editor-button')
+            edit_page.get_element(button).click()
+            edit_page.wait_until_visible((By.CLASS_NAME, "monaco-editor"))  # Wait for title field visible
+        sub_measure()
+
+
+        @print_timing("selenium_app_custom_action:submit_source_editor_content")
+        def sub_measure():
+            button = (By.XPATH, "//button[.//span//text()='Apply']")
+            edit_page.get_element(button).click()
+
+            edit_page.wait_until_invisible((By.CLASS_NAME, "monaco-editor"))  # Wait for title field visible
         sub_measure()
     measure()
