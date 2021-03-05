7cd736ef36042c7c97a6c8c69cc4ef50fa150aa6
Rytis <rytis@mohami.io>
2021-03-05 13:10:51 +0200

    dfdfdsa

--- app/extension/confluence/extension_ui.py
+++ app/extension/confluence/extension_ui.py
@@ -3,30 +3,21 @@ from selenium_ui.conftest import print_timing
 from util.conf import CONFLUENCE_SETTINGS
 
 from selenium_ui.base_page import BasePage
-from selenium_ui.confluence.pages.pages import Login, AllUpdates, PopupManager, Page, Dashboard, TopNavPanel, Editor, \
-    Logout
+
 
 def app_specific_action(webdriver, datasets):
-    edit_page = Editor(webdriver, page_id=datasets['page_id'])
+    page = BasePage(webdriver)
+    if datasets['custom_pages']:
+        app_specific_page = datasets['custom_pages']
+        app_specific_page_id = app_specific_page[0][0]
 
     @print_timing("selenium_app_custom_action")
     def measure():
-        @print_timing("selenium_app_custom_action:view_source_editor")
+
+        @print_timing("selenium_app_custom_action:view_page")
         def sub_measure():
-            edit_page.go_to()
-            edit_page.wait_for_page_loaded()
-            edit_page.wait_until_visible((By.ID, "rte-source-editor-button"))  # Wait for title field visible
-            button = (By.ID, 'rte-source-editor-button')
-            edit_page.get_element(button).click()
-            edit_page.wait_until_visible((By.CLASS_NAME, "monaco-editor"))  # Wait for title field visible
-        sub_measure()
-
-
-        @print_timing("selenium_app_custom_action:submit_source_editor_content")
-        def sub_measure():
-            button = (By.XPATH, "//button[.//span//text()='Apply']")
-            edit_page.get_element(button).click()
-
-            edit_page.wait_until_invisible((By.CLASS_NAME, "monaco-editor"))  # Wait for title field visible
+            page.go_to_url(f"{CONFLUENCE_SETTINGS.server_url}/pages/viewpage.action?pageId={app_specific_page_id}")
+            page.wait_until_visible((By.ID, "title-text"))  # Wait for title field visible
+            page.wait_until_visible((By.CLASS_NAME, "markdown-body"))  # Wait for you app-specific UI element by ID selector
         sub_measure()
     measure()
